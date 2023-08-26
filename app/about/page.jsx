f0252599aaa7c056b4a53e40bb25bd3f0c0b5504
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-08-25 23:03:00 -0300

    Primer proyecto Next

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+"use client"
+import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function About() {
+  const router = useRouter()
   return (
-    <div>Hola aca te puedo contar mas de mi</div>
-  )
+    <div>
+      <h1>Hola aca te puedo contar mas de mi</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea rerum quod blanditiis cumque rem quidem in incidunt voluptatem maxime odio quisquam possimus nemo quis itaque voluptates, praesentium nam, officia ipsam quas quos accusantium! Error, accusamus facere odio explicabo labore suscipit deserunt tenetur modi eius velit excepturi minima sapiente optio ab tempora dolor reprehenderit, incidunt culpa distinctio dolore. Eum consequatur unde perferendis ipsam dicta omnis vel modi deserunt aspernatur mollitia, earum blanditiis officiis alias optio iusto ea debitis sint quibusdam tenetur odio? Culpa earum neque nesciunt architecto, reiciendis totam dignissimos officia, nam rerum eius saepe non inventore consequatur natus quam labore?</p>
+
+      <button className="bg-sky-400 px-3 py-2 rounded-md" onClick={() => {
+        alert('Hola puedo ejecutar logica antes de reDiccionar a la pagina')
+        router.push('/')
+      }}>Clickkk</button>
+    </div>
+  );
 }
- 
